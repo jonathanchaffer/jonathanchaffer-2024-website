a51367e282ccf5d59d60956df26fec1a442316bd
Jonathan Chaffer <jonathanchaffer@gmail.com>
2024-04-15 15:27:53 -0400

    Add social links to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,11 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "@remix-run/react";
 import { FC } from "react";
 import { Container } from "./container";
 
@@ -5,10 +13,32 @@ export const Footer: FC = () => {
   return (
     <footer>
       <Container>
-        <div className="py-4 justify-center md:justify-end flex text-xs">
+        <div className="py-8 justify-center md:items-end items-center flex-col md:flex-row md:justify-between flex text-xs gap-y-8">
+          <div className="flex gap-8">
+            <SocialIcon
+              href="https://github.com/jonathanchaffer"
+              icon={faGithub}
+            />
+            <SocialIcon
+              href="https://twitter.com/jonathanchaffer"
+              icon={faTwitter}
+            />
+            <SocialIcon
+              href="https://www.linkedin.com/in/jonathan-chaffer/"
+              icon={faLinkedin}
+            />
+          </div>
           <span>© 2020—{new Date().getFullYear()} Jonathan Chaffer</span>
         </div>
       </Container>
     </footer>
+  );
+};
+
+const SocialIcon: FC<{ href: string; icon: IconProp }> = ({ href, icon }) => {
+  return (
+    <Link to={href} target="_blank" rel="noreferrer">
+      <FontAwesomeIcon icon={icon} size="2xl" />
+    </Link>
   );
 };
