e2dade0857f71bc8d776fe620071a62f9df2e155
Jonathan Chaffer <jonathanchaffer@gmail.com>
2024-04-15 12:36:25 -0400

    Add remix-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import { remixRoutes } from "remix-routes/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), remixRoutes()],
 });
