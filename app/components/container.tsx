b16c4dfeb9d9b2e5b21e2067d5f6f38be5e951b6
Jonathan Chaffer <jonathanchaffer@gmail.com>
2024-04-15 15:12:31 -0400

    Rsponsive cleanups

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 export const Container: FC<PropsWithChildren> = (props) => {
   return (
-    <div className="w-full flex justify-center px-4 md:px-8 lg:px-16">
+    <div className="w-full flex justify-center px-4 sm:px-8 md:px-16">
       <div className="max-w-screen-xl w-full">{props.children}</div>
     </div>
   );
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer: FC = () => {
   return (
     <footer>
       <Container>
-        <div className="py-4 justify-end flex text-xs">
+        <div className="py-4 justify-center md:justify-end flex text-xs">
           <span>© 2020—{new Date().getFullYear()} Jonathan Chaffer</span>
         </div>
       </Container>
